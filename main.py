69cc3e78ef890e07656bf2fc7eb5fcf540795abc
k5jm <118034837+k5jmreal@users.noreply.github.com>
2022-11-12 19:16:02 +0000

    added thing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("video","LBINTRO.mp4",.1,.1,10)
+    encoder.encode("Windoes","Windows10ScreenGame.mp4",.1,.1,10)
 
 if __name__== "__main__":
     main()
