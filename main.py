d322d06625363ef868c68c402289b409567a5747
k5jm <118034837+k5jmreal@users.noreply.github.com>
2022-11-12 21:12:41 +0000

    help

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("W10SR","Windows10Screen.GIF",.1,.1,10)
+    encoder.encode("W10SRR","ScreenWindows10.mp4",.1,.1,10)
 
 if __name__== "__main__":
     main()
