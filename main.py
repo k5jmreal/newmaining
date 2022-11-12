691e67440ca09e9ddf788e71c3b1c20c16d94169
k5jm <118034837+k5jmreal@users.noreply.github.com>
2022-11-12 20:57:13 +0000

    rr

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("Windoes","Windows10ScreenGame.mp4",.1,.1,10)
+    encoder.encode("W10S","Screen.PNG",.1,.1,10)
 
 if __name__== "__main__":
     main()
