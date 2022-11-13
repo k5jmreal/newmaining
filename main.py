22ad6626cafb995454689534cbefe4233f285d06
k5jm <118034837+k5jmreal@users.noreply.github.com>
2022-11-13 00:22:07 +0000

    rer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("Startup","BootWindows.MP4",.1,.1,10)
+    encoder.encode("fnf1","fnfvsbambitearchy.mp4",.1,.1,10)
 
 if __name__== "__main__":
     main()
